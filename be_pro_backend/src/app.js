812f0b6c54660756f8264bde53c3a57cbc225d72
ezzakyabdellah <magicalscript@hotmial.fr>
2023-08-19 23:57:27 +0100

    commit

--- be_pro_backend/src/app.js
+++ be_pro_backend/src/app.js
@@ -53,9 +53,9 @@ jobEbayRefreshDataBase.schedule();
 if (NODE_ENV === 'production') {
   console.log('NODE_ENV :::: ', NODE_ENV, NODE_ENV === 'production');
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '../be_pro_frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'be_pro_frontend/dist')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'be_pro_frontend', 'dist', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'be_pro_frontend', 'dist', 'index.html')));
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
